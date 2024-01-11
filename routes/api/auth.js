8ec0866b5285161a898f6397330629318e532ce7
ectionboy <125730248+ectionboy@users.noreply.github.com>
2024-01-11 13:48:44 +0200

    settings project

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -7,10 +7,6 @@ router.post("/register", validateBody(schemas.registerSchema), ctrl.register);
 router.post("/login", validateBody(schemas.loginSchema), ctrl.login);
 router.get("/current", authenticate, ctrl.getCurrent);
 router.post("/logout", authenticate, ctrl.logout);
-router.patch(
-  "/",
-  authenticate,
-  validateBody(schemas.patchSubscriptionSchema),
-  ctrl.patchSubscription
+router.patch("/", authenticate, validateBody(schemas.patchSubscriptionSchema),ctrl.patchSubscription
 );
 module.exports = router;
