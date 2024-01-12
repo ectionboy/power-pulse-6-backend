f25f873e4b40e924eff3900752899da62ae226eb
Oleksandr Tsurkan <sanyaczurkan2000@gmail.com>
2024-01-12 18:33:51 +0200

    fix 204 res in logout

--- controllers/auth/logout/logout.js
+++ controllers/auth/logout/logout.js
@@ -4,9 +4,9 @@ const { ctrlWrapper } = require("../../../helpers");
 const logout = async (req, res) => {
   const { _id } = req.user;
   await User.findByIdAndUpdate(_id, { token: "" });
-  res.status(200).json({
+  res.status(204).json({
     message: "No Content",
   });;
 };
-// if tou want to receive the res 204 please change
+
 module.exports = ctrlWrapper(logout);
