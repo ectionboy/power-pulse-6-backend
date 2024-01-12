4c3dece0c9a980eccfe50a862d800083982b7c88
Oleksandr Tsurkan <sanyaczurkan2000@gmail.com>
2024-01-12 12:48:52 +0200

    auth-controllers

--- controllers/auth/register/register.js
+++ controllers/auth/register/register.js
@@ -1,26 +1,31 @@
 const bcrypt = require("bcrypt");
+const gravatar = require("gravatar");
 
 const { User } = require("../../../models/user");
 
-const { HttpError } = require("../../../helpers");
+const { HttpError, ctrlWrapper } = require("../../../helpers");
 
 const register = async (req, res) => {
-    const { email, password } = req.body;
-    const user = await User.findOne({ email });
-  
-    if (user) {
-      throw HttpError(409, "Email already in use");
-    }
-  
-    const hashPassword = await bcrypt.hash(password, 10);
-  
-    const newUser = await User.create({ ...req.body, password: hashPassword });
-    res.status(201).json({
-      user: {
-        email: newUser.email,
-        subscription: newUser.subscription,
-      },
-    });
-  };
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
 
-  module.exports = register;
+  if (user) {
+    throw HttpError(409, "Email already in use");
+  }
+  const avatarURL = gravatar.url(email, { s: "250" });
+  const hashPassword = await bcrypt.hash(password, 10);
+  const newUser = await User.create({
+    ...req.body,
+    password: hashPassword,
+    avatarURL,
+  });
+  res.status(201).json({
+    user: {
+      name: newUser.name,
+      email: newUser.email,
+      avatarURL: newUser.avatarURL,
+    },
+  });
+};
+
+module.exports = ctrlWrapper(register);
---
+++ controllers/auth/updateAvatar/updateAvatar.js
@@ -0,0 +1,23 @@
+const fs = require("fs/promises");
+const path = require("path");
+
+const { ctrlWrapper, normalizeAvatar } = require("../../../helpers");
+
+const { User } = require("../../../models/user");
+
+const avatarStoragePath = path.join(__dirname, "../../../", "public", "avatars");
+
+const updateAvatar = async (req, res) => {
+  const { path: tempAvatar, filename } = req.file;
+  await normalizeAvatar(tempAvatar, 250);
+  const { _id } = req.user;
+  const avatarURL = path.join("avatars", filename);
+  const avatarPath = path.join(avatarStoragePath, filename);
+  await fs.rename(tempAvatar, avatarPath);
+  await User.findByIdAndUpdate(_id, { avatarURL });
+  res.status(200).json({
+    avatarURL,
+  });
+};
+
+module.exports = ctrlWrapper(updateAvatar);
