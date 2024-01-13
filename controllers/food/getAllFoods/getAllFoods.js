889c34feae7e979f00f0a20e03d10bbe36ec1a19
ectionboy <125730248+ectionboy@users.noreply.github.com>
2024-01-13 20:41:13 +0200

    Update getAllFoods.js

--- controllers/food/getAllFoods/getAllFoods.js
+++ controllers/food/getAllFoods/getAllFoods.js
@@ -5,18 +5,8 @@ const getAllFoods = async (req, res) => {
 	const { page = 1, limit = 16 } = req.query;
 	const skip = (page - 1) * limit;
 
-	const category = {};
-	if (req.query.bodyPart) {
-		category.bodyPart = req.query.bodyPart;
-	}
-	if (req.query.equipment) {
-		category.equipment = req.query.equipment;
-	}
-	if (req.query.target) {
-		category.target = req.query.target;
-	}
-	const data = await Food.find(category).skip(skip).limit(limit);
-	const total = await Food.countDocuments(category);
+	const data = await Food.find().skip(skip).limit(limit);
+	const total = await Food.countDocuments();
 	if (!data) {
 		throw HttpError(404, "Foods not found");
 	}
