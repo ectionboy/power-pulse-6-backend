8f4bbdc0cf3f737460f5846c0943f500b2391836
DenysVM <muratov.d@gmail.com>
2024-01-18 16:23:38 +0100

    diary-03

--- controllers/diary/getDiaryByDate/getDiaryByDate.js
+++ controllers/diary/getDiaryByDate/getDiaryByDate.js
@@ -4,8 +4,11 @@ const dateToShortFormat = require('../../../helpers/dateToShortFormat');
 
 async function getDiaryByDate(req, res, next) {
     const { userId, date } = req.params;
-
+    const currentUserId = req.user.id;
     try {
+        if (userId !== currentUserId) {
+            throw HttpError(403, 'Access denied');
+        }
         const diary = await Diary.findOne({ user: userId, date })
             .populate({
                 path: 'products.productId',
