9babf9d0057bd637b2e0ca5bb7d9593fb4790b2f
Oleksandr Tsurkan <sanyaczurkan2000@gmail.com>
2024-01-18 23:22:43 +0200

    fix-token+current-user

--- controllers/profile/getBmr/getBmr.js
+++ controllers/profile/getBmr/getBmr.js
@@ -2,18 +2,18 @@ const { Profile } = require("../../../models/profile");
 const { ctrlWrapper, HttpError } = require("../../../helpers");
 
 const getBmr = async (req, res) => {
-  const { _id: id } = req.user;
+  // const { _id: id } = req.user;
 
-  const result = await Profile.findOne({ owner: id });
-  if (!result) {
-    throw HttpError(404, "Not found");
-  }
-  const targetBmr = result.bmr;
-  const targetTime = 110;
-  res.json({
-    targetBmr,
-    targetTime,
-  });
+  // const result = await Profile.findOne({ owner: id });
+  // if (!result) {
+  //   throw HttpError(404, "Not found");
+  // }
+  // const targetBmr = result.bmr;
+  // const targetTime = 110;
+  // res.json({
+  //   targetBmr,
+  //   targetTime,
+  // });
 };
 
 module.exports = ctrlWrapper(getBmr);
