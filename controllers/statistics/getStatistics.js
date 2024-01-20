1c546be96db1eeaabd7bb30a63ca8884879b26ef
Oleksandr Tsurkan <sanyaczurkan2000@gmail.com>
2024-01-20 15:50:55 +0200

    statistics

---
+++ controllers/statistics/getStatistics.js
@@ -0,0 +1,37 @@
+const { ctrlWrapper } = require("../../helpers");
+const { User } = require("../../models/user");
+const { Exercise } = require("../../models/training");
+const { Diary } = require("../../models/diary");
+
+const getStatistics = async (req, res) => {
+  const aggregationPipeline = [
+    {
+      $group: {
+        _id: Date.now(),
+        workoutCount: { $sum: 1 },
+        burnedCaloriesSum: { $sum: "$burnedCalories" },
+        workoutsTimeSum: { $sum: "$time" },
+      },
+    },
+  ];
+
+  const statistics = await Exercise.aggregate(aggregationPipeline);
+
+  if (statistics.length > 0) {
+    const userCount = await User.countDocuments();
+    const videoCount = await Diary.countDocuments();
+    res.json({ ...statistics[0], userCount, videoCount });
+  } else {
+    const userCount = await User.countDocuments();
+    const videoCount = await Diary.countDocuments();
+    res.json({
+      userCount,
+      videoCount,
+      workoutCount: 0,
+      burnedCaloriesSum: 0,
+      workoutsTimeSum: 0,
+    });
+  }
+};
+
+module.exports = ctrlWrapper(getStatistics);
