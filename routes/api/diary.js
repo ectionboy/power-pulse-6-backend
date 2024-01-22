7da05159bbbd4f59ff92610bdd56a0e0d5ba6560
ectionboy <125730248+ectionboy@users.noreply.github.com>
2024-01-22 21:28:14 +0200

    Revert "Diary-04"

--- routes/api/diary.js
+++ routes/api/diary.js
@@ -4,11 +4,11 @@ const diaryController = require('../../controllers/diary');
 const authenticate = require('../../middlewares/authenticate');
 const validateBody = require('../../middlewares/validateBody');
 
-const { diaryEntrySchema } = require('../../models/diary');
+const { diaryEntrySchema, deleteEntrySchema } = require('../../models/diary');
 
 router.post('/add-entry', authenticate, validateBody(diaryEntrySchema), diaryController.addEntry);
 
-router.delete('/delete-entry/:userId/:date/:entryId', authenticate, diaryController.deleteEntry);
+router.delete('/delete-entry', authenticate, validateBody(deleteEntrySchema), diaryController.deleteEntry);
 
 router.get('/:userId/:date', authenticate, diaryController.getDiaryByDate);
 
